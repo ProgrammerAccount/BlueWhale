ab16692748f3c2b9e83def12d51e6da803413fca
Tymoteusz <td.janiak@gmail.com>
2017-05-06 17:00:55 +0200

    seawavev2

--- main.js
+++ main.js
@@ -3,32 +3,6 @@ var ctx= canvas.getContext("2d");
 var x1=-400,x2=0,x3=400,x4=-800;
 var i=0;
 
-function wave(startX)
-{
-ctx.moveTo(startX,650);
-ctx.quadraticCurveTo(startX+400,400,startX+800,650);
-	
-}
-function draw()
-{
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-ctx.beginPath();
-x1++;
-x2++;
-x3++;
-x4++;
-if(x1<800) wave(x1);
-	else x1=-800;
-if(x2<800)	wave(x2);
-	else x2=-800;
-if(x3<800) wave(x3);
-	else x3=-800;
-if(x4<800) wave(x4);
-	else x4=-800;
 
-ctx.fillStyle="#33ccff";
-ctx.fill();
-
-}
-setInterval(draw,10);
+setInterval(drawWave,10);
 //draw();
--- sea.js
+++ sea.js
@@ -1,8 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* global ctx, canvas */
 
+function wave(startX)
+{
+ctx.moveTo(startX,650);
+ctx.quadraticCurveTo(startX+400,400,startX+800,650);
+	
+}
+function drawWave()
+{
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.beginPath();
+x1++;
+x2++;
+x3++;
+x4++;
+if(x1<800) wave(x1);
+	else x1=-800;
+if(x2<800)	wave(x2);
+	else x2=-800;
+if(x3<800) wave(x3);
+	else x3=-800;
+if(x4<800) wave(x4);
+	else x4=-800;
 
+ctx.fillStyle="#33ccff";
+ctx.fill();
 
+}
